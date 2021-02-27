8018a8422b97b9ad99a815e64c8e5256af6b2a64
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-02-26 23:28:05 -0500

    Removed extra comments

--- lib/screens/price_screen.dart
+++ lib/screens/price_screen.dart
@@ -124,9 +124,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  // '1 $selectedCryptoValueAssetBase = $currencyAmount $selectedCurrencyValueAssetQuote',
                   '$cryptoAmount $selectedCryptoValueAssetBase = ${currencyFormat.format(currencyAmount)} $selectedCurrencyValueAssetQuote',
-                  // '1 $selectedCryptoValueAssetBase = ${currencyFormat.format(132323.4343)} $selectedCurrencyValueAssetQuote',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
