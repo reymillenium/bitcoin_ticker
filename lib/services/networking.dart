40a48f5e352397b6e5231a744aa18377547a4b9a
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-03-22 03:51:09 -0400

    Modified the method getSpecificExchangeRateData at lib/services/networking.dart, by using the new rest-api-coinfree instead of the Coin AOI directly

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -8,7 +8,8 @@ import 'package:bitcoin_ticker/utilities/constants.dart';
 class NetworkHelper {
   // Gets the exchange rate data
   Future getSpecificExchangeRateData({String assetIdBase, String assetIdQuote}) async {
-    String specificExchangeRateURL = '$coinApiExchangeRateURL/$assetIdBase/$assetIdQuote?apikey=$freeCoinApiKey';
+    // String specificExchangeRateURL = '$coinApiExchangeRateURL/$assetIdBase/$assetIdQuote?apikey=$freeCoinApiKey';
+    String specificExchangeRateURL = 'https://rest-api-freecoin.herokuapp.com/api/v1/general_exchange_${assetIdBase.toLowerCase()}_rows/general_exchange_${assetIdBase.toLowerCase()}?asset_id_quote=$assetIdQuote';
     // print(specificExchangeRateURL);
     return await _getData(url: specificExchangeRateURL);
   }
